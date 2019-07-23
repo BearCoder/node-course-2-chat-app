2615b3fcd97e5cd3bd301bee678e2c64ebf1922e
BearCoder <cosmin.z21@gmail.com>
2019-07-23 12:44:00 +0200

    'added'

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io= socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        });
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
